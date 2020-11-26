ba9c9442923531b36199895671cc1c6669aaa06a
Ostap Mykhailivkiy <djtomsawyer1@gmail.com>
2020-11-26 13:25:05 +0200

    Initial commit

---
+++ src/Homework.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Homework {
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Specify the radius: ");
+//        double r = scanner.nextDouble();
+//        double area = Math.PI * Math.pow(r,2);
+//        double per = 2 * Math.PI * r;
+//        System.out.printf("Flower bed's area is %f , perimeter is %f ", area, per);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Hey!What's your name?");
+//        String name = scanner.nextLine();
+//        System.out.printf("Ok,Where are you live, %s ?\n", name);
+//        String address = scanner.nextLine();
+//        System.out.printf("Your name is: %s \nYour living address is: %s", name,address);
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("What is the price of phone call in country 1?");
+//        double c1 = scanner.nextDouble();
+//        System.out.println("What was the  duration of first telephone call?");
+//        double t1 = scanner.nextDouble();
+//        System.out.println("What is the price of phone call in country 2?");
+//        double c2 = scanner.nextDouble();
+//        System.out.println("What was the  duration of second telephone call?");
+//        double t2 = scanner.nextDouble();
+//        System.out.println("What is the price of phone call in country 3?");
+//        double c3 = scanner.nextDouble();
+//        System.out.println("What was the third duration of telephone call?");
+//        double t3 = scanner.nextDouble();
+//        double p1 = c1*t1;
+//        System.out.println("The price for call in country 1 was " + p1 + "units");
+//        double p2 = c2*t2;
+//        System.out.println("The price for call in country 2 was " + p2 + "units");
+//        double p3 = c3*t3;
+//        System.out.println("The price for call in country 3 was " + p3 + "units");
+//        double sum = p1+p2+p3;
+//        System.out.println("Total price for all conversations vas: " + sum);
+
+    }
+}
